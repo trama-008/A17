2f7ac128e09dd7d6497cc75848935c6f02ac7c78
SORAxSHIRO <76656461+SORAxSHIRO@users.noreply.github.com>
2023-11-24 19:27:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
+global.Owner = ["249119105711", "249119105711"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["249119105711", "249119105711"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["249119105711"];
+global.OwnerName = "SORA";
+global.BotName = "SHIRO";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
