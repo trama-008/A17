e2457e238f38370e5f065f307c6278d97ba8e611
SORAxSHIRO <76656461+SORAxSHIRO@users.noreply.github.com>
2023-11-25 14:50:31 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -5544,7 +5544,7 @@ _Click the button below to download_`
         if (isBanChat) return reply(mess.bangc);
         A17.sendMessage(from, { react: { text: "🫡", key: m.key } })
 
-        if (!args.join(" ")) return reply(`Like use -take SHIRO|By: SORA`)
+if (!args.join(" ")) return reply(`Like use -take A17|By: Kai`)
         const swn = args.join(" ")
         const pcknm = swn.split("|")[0];
         const atnm = swn.split("|")[1];
