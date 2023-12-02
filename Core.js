8f6c8ad57fea51c5587be0b9679b160477372d4e
SORAxSHIRO <76656461+SORAxSHIRO@users.noreply.github.com>
2023-12-02 14:51:46 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -3788,7 +3788,7 @@ if (smallinput.includes('شيرو لنتزوج')|| smallinput.includes('شيرو
         } else if (args[0] === 'open') {
           await A17.groupSettingUpdate(m.chat, 'not_announcement').then((res) => reply(`Group has been opened!`)).catch((err) => reply(jsonformat(err)))
         } else {
-*/
+
           let buttonMessage = {
             image: BotLogo,
             jpegThumbnail: Thumb,
@@ -3800,7 +3800,7 @@ if (smallinput.includes('شيرو لنتزوج')|| smallinput.includes('شيرو
         }
       }
         break;
-
+*/
 
       case 'promote': case 'admin': {
         if (isBan) return reply(mess.banned);
