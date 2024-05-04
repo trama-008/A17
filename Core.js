d7c6b102236ba7f8ce9eddbab0ce01e927f06b87
SORAxSHIRO <76656461+SORAxSHIRO@users.noreply.github.com>
2024-05-04 11:31:21 +0200

    Update Core.js

--- Core.js
+++ Core.js
@@ -832,12 +832,6 @@ if (smallinput.includes('شيرو لنتزوج')|| smallinput.includes('شيرو
       }
         break;
 
-case "America ya?" :{
-        For(var i=0; i<25; i++) 
-conn.sendmessage(m.chat, {text: "HELLO :D"}) 
- } 
-                 break;
-
       case 'qt': {
         if (!args[0] && !m.quoted) {
           return m.reply(`Please provide a text (Type or mention a message) !`);
