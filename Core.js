59fc4ab46c660318c9ee302341a293df7e90178d
trama-008 <tramaman003@gmail.com>
2024-07-31 23:59:22 +0300

    Update Core.js

--- Core.js
+++ Core.js
@@ -262,7 +262,7 @@ module.exports = A17 = async (A17, m, chatUpdate, store) => {
     const pushname = m.pushName || "No Name"
     const botNumber = await A17.decodeJid(A17.user.id)
     const isCreator = [botNumber, ...global.Owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
-    const isyuki = [botNumber, ...global.yukio].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+    const isyuki = [botNumber, ...global.yuki].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
     const itsMe = m.sender == botNumber ? true : false
     const text = args.join(" ")
     const from = m.chat
